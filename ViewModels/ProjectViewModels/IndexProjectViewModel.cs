871ce289d9589ce4bdd51e7481500b7ae7cafb23
Gumbinnen <personal.gusev@gmail.com>
2024-02-23 02:13:39 +0500

    Add ProjectViewModels/

---
+++ ViewModels/ProjectViewModels/IndexProjectViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BugTrackingSystem.Models.Entities;
+
+namespace BugTrackingSystem.ViewModels.ProjectViewModels
+{
+    public class IndexProjectViewModel
+    {
+        public IEnumerable<Project> Projects { get; set; }
+
+        public IndexProjectViewModel(IEnumerable<Project> projects)
+        {
+            this.Projects = projects;
+        }
+    }
+}
---
+++ ViewModels/ProjectViewModels/ProjectViewModel.cs
@@ -0,0 +1,31 @@
+﻿using BugTrackingSystem.Models.Entities;
+using BugTrackingSystem.Models.LinkingEntities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugTrackingSystem.ViewModels.ProjectViewModels
+{
+    public class ProjectViewModel
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public PersonalSpace PersonalSpace { get; set; }
+        public IEnumerable<Bug> Bugs { get; set; }
+        public IEnumerable<ApplicationRole> CreatedRoles { get; set; }
+        public IEnumerable<ApplicationProjectUserRole> ProjectUserRoles { get; set; }
+
+        public ProjectViewModel()
+        {
+            
+        }
+
+        public ProjectViewModel(Project project) 
+        {
+            Name = project.Name;
+            Description = project.Description;
+            PersonalSpace = project.PersonalSpace;
+            Bugs = project.Bugs;
+            CreatedRoles = project.CreatedRoles;
+            ProjectUserRoles = project.ProjectUserRoles;
+        }
+    }
+}
