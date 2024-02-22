20e71302f1fe5e2f3aa4053856ef61ce84b59644
Gumbinnen <personal.gusev@gmail.com>
2024-02-23 02:14:51 +0500

    Add EnumUtility.cs and PasswordHasher.cs (BCrypt)

---
+++ Helpers/EnumUtility.cs
@@ -0,0 +1,10 @@
+﻿namespace BugTrackingSystem.Helpers
+{
+    public class EnumUtility
+    {
+        public static IEnumerable<T> GetValues<T>()
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
+        }
+    }
+}
---
+++ Helpers/PasswordHasher.cs
@@ -0,0 +1,21 @@
+﻿using BugTrackingSystem.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using BC = BCrypt.Net.BCrypt;
+
+namespace BugTrackingSystem.Helpers
+{
+    public class PasswordHasher : IPasswordHasher<ApplicationUser>
+    {
+        private const int WorkFactor = 11;
+
+        public string HashPassword(ApplicationUser user, string password)
+        {
+            return BC.EnhancedHashPassword(password, WorkFactor);
+        }
+
+        public PasswordVerificationResult VerifyHashedPassword(ApplicationUser user, string hashedPassword, string providedPassword)
+        {
+            return BC.EnhancedVerify(providedPassword, hashedPassword) ? PasswordVerificationResult.Success : PasswordVerificationResult.Failed;
+        }
+    }
+}
